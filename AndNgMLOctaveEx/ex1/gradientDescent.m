436ec1f535469c8c4868d8bd6eebfff5f07036f3
iamcuigang <37521971+iamcuigang@users.noreply.github.com>
2018-04-18 20:00:55 +0800

    disable warning

--- AndNgMLOctaveEx/ex1/gradientDescent.m
+++ AndNgMLOctaveEx/ex1/gradientDescent.m
@@ -16,10 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-    delta = alpha/m * sum((X*theta-y).*X)';
-    theta = theta - delta;
-
+    d = X*theta-y;
+    delta = [d, d];
+    theta = theta - alpha/m * sum(delta.*X)';
 
     % ============================================================
 
