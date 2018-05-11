75ec87f7e37ebef5812f80e1cb229347b1492726
iamcuigang <37521971+iamcuigang@users.noreply.github.com>
2018-05-11 14:32:03 +0800

    ex5 70%

--- AndNgMLOctaveEx/ex5/linearRegCostFunction.m
+++ AndNgMLOctaveEx/ex5/linearRegCostFunction.m
@@ -7,7 +7,6 @@ function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
 
 % Initialize some useful values
 m = length(y); % number of training examples
-
 % You need to return the following variables correctly 
 J = 0;
 grad = zeros(size(theta));
@@ -19,18 +18,25 @@ grad = zeros(size(theta));
 %               You should set J to the cost and grad to the gradient.
 %
 
-theta_but0 = theta(2:end);
-J1 = 1/(2*m)*sum( (X*theta - y).^2);
-J2 = lambda / (2*m) * sum(theta_but0.^2);
-J = J1 + J2;
+%theta_but0 = theta(2:end);
+%J1 = 1/(2*m)*sum( (X*theta - y).^2);
+%J2 = lambda / (2*m) * sum(theta_but0.^2);
+%J = J1 + J2;
 
-d = (X*theta-y);
-delta = repmat(d, 1, size(X, 2));
+J = (1/(2*m)) * sumsq(X*theta-y);
+reg = lambda/(2*m)*sumsq(theta(2:end));
+J += reg;
 
-g1 = 1/m * sum( delta .* X )';
-theta(1) = 0;
-g2 = lambda / m .* theta;
-grad = g1 + g2;
+%d = (X*theta-y);
+%delta = repmat(d, 1, size(X, 2));
+%
+%g1 = 1/m * sum( delta .* X )';
+%theta(1) = 0;
+%g2 = lambda / m .* theta;
+%grad = g1 + g2;
+
+grad = (X' * (X*theta-y)) / m;
+grad(2:end) += lambda * theta(2:end) / m;
 % =========================================================================
 
 grad = grad(:);
