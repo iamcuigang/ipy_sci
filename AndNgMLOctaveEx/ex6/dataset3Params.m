e9dbb4b5d23ed0cf959c62d878e8d421fc986e65
^^SNP^^ <37521971+iamcuigang@users.noreply.github.com>
2018-05-17 23:42:11 +0800

    ex6 50%

--- AndNgMLOctaveEx/ex6/dataset3Params.m
+++ AndNgMLOctaveEx/ex6/dataset3Params.m
@@ -8,8 +8,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %
 
 % You need to return the following variables correctly.
-C = 1;
-sigma = 0.3;
+C = 0.3; % 0.3, 1, 3
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,11 +23,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+%List = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+%error = zeros([length(List), length(List)]);
+%n = length(List);
 
-
-
-
-
+%for i=1:n
+%    for j = 1:n
+%        C = List(i);
+%        sigma = List(j);
+%        model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+%        predictions = svmPredict(model, Xval);
+%        error(i, j) = mean(double(predictions~=yval));
+%    end
+%end
+%disp(error);
 
 % =========================================================================
 
